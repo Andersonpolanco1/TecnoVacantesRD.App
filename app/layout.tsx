5ab2aed8018e68cbc23b5c8d24bb8e8232124f14
Anderson Polanco <apolancoc@dgii.gov.do>
2025-03-16 19:30:53 -0400

    chned bootstrap to tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { getSession } from "@/app/api/auth/authOptions";
 import Navbar from "@/components/Navbar";
 import AppSessionProvider from "../providers/appSessionProvider";
 import { NotificationProvider } from "@/providers/notificationProvider";
 import ScrollToUpButton from "@/components/ScrollToUpButton";
 import { LoadingProvider } from "@/providers/loadingProvider";
-import "./styles/style.css";
+import "../globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
